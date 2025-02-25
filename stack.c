346ecfd5150de8833ec89b2045fc3fe99c39377f
Michael Peng <michael@mcmoo.org>
2025-02-25 09:43:57 -0800

    Implement push

--- stack.c
+++ stack.c
@@ -12,3 +12,9 @@ void reset() {
 
 // Implement your team's function here!
 
+void push(int i) {
+    struct Node *new = malloc(sizeof(struct Node));
+    new->data = i;
+    new->next = stackTop;
+    stackTop = new;
+}
